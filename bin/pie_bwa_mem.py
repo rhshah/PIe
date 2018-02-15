3bb4ddb59efde1fa287f9129362680c99a2b8586
Ronak Shah <rons.shah@gmail.com>
2018-02-15 17:34:17 -0500

    change help

--- bin/pie_bwa_mem.py
+++ bin/pie_bwa_mem.py
@@ -100,7 +100,8 @@ USAGE
         "--read_group",
         dest="read_group",
         type=str,
-        help="information regarding read group for SAM file within quotes(ex: \"\'@RG\\tID:1\\tSM:1247014_S5\\tLB:1247014_S5_L001\\tPL:ILLUMINA\'\")[required]")
+        help="information regarding read group for SAM file within quotes \
+        (ex: \"\'@RG\\tID:1\\tSM:1247014_S5\\tLB:1247014_S5_L001\\tPL:ILLUMINA\'\")")
     parser.add_argument(
         "-o",
         "--output",
