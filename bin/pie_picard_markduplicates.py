2d5f0d723897f21a73a2d865ffd23873359eac31
Ronak Shah <rons.shah@gmail.com>
2018-04-13 09:41:05 -0400

    Removing script

--- bin/pie_picard_markduplicates.py
+++ bin/pie_picard_markduplicates.py
@@ -36,7 +36,53 @@ __version__ = '.'.join(__version_info__)
 __date__ = '2018-02-12'
 __updated__ = '2018-02-15'
 
-
+def picard_std_args(parser):
+    parser.add_argument(
+        "-tmp",
+        "--tmp_dir",
+        dest="tmp_dir",
+        default="/scratch/",
+        help="path to the temporary directory")
+    parser.add_argument(
+        "-s",
+        "--validation_stringency",
+        dest="validation_stringency",
+        default="SILENT",
+        help=
+        "Validation stringency for all SAM files read by this program. Setting stringency to SILENT can improve performance when processing a BAM file in which variable-length data (read, qualities, tags) do not otherwise need to be decoded.[default=SILENT]"
+    )
+    parser.add_argument(
+        "-c",
+        "--compression_level",
+        dest="compression_level",
+        default="5",
+        help=
+        "Compression level for all compressed files created (e.g. BAM and GELI).[default=5]"
+    )
+    parser.add_argument(
+        "-ci",
+        "--create_index",
+        dest="create_index",
+        default="true",
+        help=
+        "Whether to create a BAM index when writing a coordinate-sorted BAM file.[default=true]"
+    )
+    parser.add_argument(
+        "-cm",
+        "--create_md5_file",
+        dest="create_md5_file",
+        default="false",
+        help=
+        "Whether to create an MD5 digest for any BAM or FASTQ files created.[default=false]"
+    )
+    parser.add_argument(
+        "-ref",
+        "--reference_sequence",
+        dest="reference_sequence",
+        choices=pie.util.genomes.keys(),
+        required=True,
+        help="Reference sequence file.[required]")
+    return (parser)
 
 # process the given arguments from the command line
 def process_command_line(argv):
@@ -69,7 +115,7 @@ USAGE
     parser = ArgumentParser(
         description=program_license,
         formatter_class=RawDescriptionHelpFormatter)
-    parser = pie.standard_args.picard_std_args(parser)
+    parser = picard_std_args(parser)
     # define options here:
     parser.add_argument(
         "-p",
